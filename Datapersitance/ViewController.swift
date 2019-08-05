034cad6d459e8d082037b8a1bc04c0f2547e87b4
Phani_Kiran <pret321@gmail.com>
2019-08-04 23:15:03 -0500

    Saving number and updating the contact is been added.

--- Datapersitance/ViewController.swift
+++ Datapersitance/ViewController.swift
@@ -9,11 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var number: UITextField!
+    @IBOutlet weak var buttonOne: UIButton!
 
+    var nameTwo: String = ""
+    var numberTwo: String = ""
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        nameTwo = UserDefaults.standard.value(forKey: "OneName") as! String
+        numberTwo = UserDefaults.standard.value(forKey: "OneNumber") as! String
+        print(numberTwo)
+
+        if (name.text == "" && number.text == ""){
+            buttonOne.setTitle("save", for: .normal)
+            name.text = ""
+            number.text = ""
+        }else{
+            buttonOne.setTitle("update", for: .normal)
+            name.text = nameTwo
+            number.text = numberTwo
+        }
+        
+ 
+
+
+        
+        
+
     }
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        UserDefaults.standard.set(name.text!, forKey: "OneName")
+        UserDefaults.standard.set(number.text!, forKey: "OneNumber")
+
+        
+    }
+    
 
 
 }
